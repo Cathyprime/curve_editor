b5444749185231b13c18bb0f8ccf06ad076ba168
Cathyprime <magdajaroszynska47@gmail.com>
2024-08-23 11:37:47 +0200

    update build system

--- nob_stage2.cpp
+++ nob_stage2.cpp
@@ -15,11 +15,11 @@ namespace ranges = std::ranges;
 void help()
 {
 	std::cout << std::endl;
-	std::cout << "[HELP]: provide one of the arguments:" << std::endl;
-	std::cout << "[HELP]:	-> run - compile and run the app" << std::endl;
-	std::cout << "[HELP]:	-> help - display help" << std::endl;
-	std::cout << "[HELP]:	-> build - compile the app" << std::endl;
-	std::cout << "[HELP]:	-> clean - remove build artefacts" << std::endl;
+	std::cout << "[HELP] provide one of the arguments:" << std::endl;
+	std::cout << "[HELP]	-> run - compile and run the app" << std::endl;
+	std::cout << "[HELP]	-> help - display help" << std::endl;
+	std::cout << "[HELP]	-> build - compile the app" << std::endl;
+	std::cout << "[HELP]	-> clean - remove build artefacts" << std::endl;
 }
 
 int main(int argc, char** argv)
